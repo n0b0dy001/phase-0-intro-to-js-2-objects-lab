87a3777e6f1b612d1fb1d740f952d1950f778ce9
Christian Reyna <christianreyna12@gmail.com>
2023-08-25 07:41:00 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+const employee = {
+    name: 'John',
+    streetAddress: '123 Main St'
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Use spread operator to create a new object with updated key-value pair
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Mutate the original employee object
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object with the specified key removed
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Mutate the original employee object
+    delete employee[key];
+    return employee;
+  }
+  
+  module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+  };
